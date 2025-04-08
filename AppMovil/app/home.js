1c3b5468524895530417e0ba218ea3fb0369adba
emarojass05 <e84671818@gmail.com>
2025-04-08 15:54:36 -0600

    Add: Cuenta

--- AppMovil/app/home.js
+++ AppMovil/app/home.js
@@ -1,14 +1,33 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, ScrollView } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Bienvenido a TecBank 🏦</Text>
-    </View>
-  );
+    <ScrollView contentContainerStyle={styles.container}>
+  <Text style={styles.title}>Bienvenido a TecBank 🏦</Text>
+
+  <Button title="Ver Cuentas" onPress={() => router.push('/cuentas')} />
+  <Button title="Ver Tarjetas" onPress={() => router.push('/tarjetas')} />
+  <Button title="Ver Trámites" onPress={() => router.push('/tramites')} />
+  <Button title="Ver Préstamos" onPress={() => router.push('/prestamos')} />
+  <Button title="Cerrar sesión" onPress={() => router.replace('/')} />
+</ScrollView>
+);
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 22 },
+container: {
+padding: 30,
+flexGrow: 1,
+justifyContent: 'center',
+gap: 20,
+},
+title: {
+fontSize: 26,
+textAlign: 'center',
+marginBottom: 30,
+fontWeight: 'bold',
+},
 });
